63fb763e3784b7c5cd2be3b48676c70e98a87eb6
j1y2b3 <2162539907@qq.com>
2025-02-02 20:13:51 +0800

    更改文件

--- pack.py
+++ pack.py
@@ -1,81 +1,140 @@
+import os
+import shutil
 import subprocess
-import sys
-import PyInstaller.__main__
+#import PyInstaller.__main__
 
 
-def Pack(pythonfile: str, *,
-         path: str | None = None,
-         name: str | None = None,
-         icon: str | None = None,
-         nonconsole: bool = False,
-         filestyle: str = 'onedir', # 或 onefile
-         distpath: str | None = None,
-         workpath: str | None = None,
-         specpath: str | None = None,
-         coverdist: bool = False,
-         coverwork: bool = False,
-         loglevel: str = 'ERROR', # 或 TRACE DEBUG INFO WARN ERROR CRITICAL
-         datas: list[(str, str)] = [], # [(编译时文件位置, 运行时文件位置), ...]
-         binaries: list[(str, str)] = [], # 同上
-         moreimportpaths: list[str] = [], # 额外导入的包
-         morehiddenimports: list[str] = [], # 额外隐藏导入的包
-         excludemodules: list[str] = [], # 排除的包
-         runtimetmpdir: str | None = None # 运行时临时目录，用于onefile且有datas或binary时
-         ):
+class Pack:
 
-    commands = [pythonfile]
+    def __init__(self,
+                 pythonfile: str, *,
+                 path: str | None = None,
+                 name: str | None = None,
+                 icon: str | None = None,
+                 nonconsole: bool = False,
+                 filestyle: str = 'onedir', # 或 onefile
+                 distpath: str | None = None,
+                 workpath: str | None = None,
+                 specpath: str | None = None,
+                 coverdist: bool = False,
+                 coverwork: bool = False,
+                 loglevel: str = 'INFO', # 或 TRACE DEBUG INFO WARN ERROR CRITICAL
+                 datas: list[(str, str)] = [], # [(编译时文件位置, 运行时文件位置), ...]
+                 binaries: list[(str, str)] = [], # 同上
+                 moreimportpaths: list[str] = [], # 额外导入的包
+                 morehiddenimports: list[str] = [], # 额外隐藏导入的包
+                 excludemodules: list[str] = [], # 排除的包
+                 runtimetmpdir: str | None = None # 运行时临时目录，用于onefile且有datas或binary时
+                 ):
+        '''生成完整的应用程序'''
+        
+        self.pythonfile = pythonfile
+        self.path = path
+        self.name = name
+        self.icon = icon
+        self.nonconsole = nonconsole
+        self.filestyle = filestyle
+        self.distpath = distpath
+        self.workpath = workpath
+        self.specpath = specpath
+        self.coverdist = coverdist
+        self.coverwork = coverwork
+        self.loglevel = loglevel
+        self.datas = datas
+        self.binaries = binaries
+        self.moreimportpaths = moreimportpaths
+        self.morehiddenimports = morehiddenimports
+        self.excludemodules = excludemodules
+        self.runtimetmpdir = runtimetmpdir
 
-    if name is not None:
-        commands.append('-n ' + name)
-    if icon is not None:
-        commands.append('-i ' + icon)
-    if nonconsole:
-        commands.append('--noconsole')
+    def pack(self):
+        '''用pyinstaller打包exe文件'''
 
-    if filestyle == 'onefile':
-        commands.append('--onefile')
-    elif filestyle == 'onedir':
-        commands.append('--onedir')
-    else:
-        raise ValueError('filestyle must be onefile or onedir')
+        # 生成命令
+        self.commands = [self.pythonfile]
 
-    if distpath is not None:
-        commands.append('--distpath ' + distpath)
-    if workpath is not None:
-        commands.append('--workpath ' + workpath)
-    if specpath is not None:
-        commands.append('--specpath ' + specpath)
+        self.cwd={}
+        if self.path is not None:
+            self.cwd['cwd'] = self.path
 
-    if coverdist:
-        commands.append('-y')
-    if coverwork:
-        commands.append('-clean')
-    
-    if loglevel != 'INFO':
-        commands.append('--log-level ' + loglevel)
+        if self.name is not None:
+            self.commands.append('-n ' + self.name)
+        if self.icon is not None:
+            self.commands.append('-i ' + self.icon)
+        if self.nonconsole:
+            self.commands.append('--noconsole')
 
-    if datas:
-        for data in datas:
-            commands.append(f'--add-data "{data[0]};{data[1]}"')
-    if binaries:
-        for binary in binaries:
-            commands.append(f'--add-binary "{binary[0]};{binary[1]}"')
-    
-    if moreimportpaths:
-        for importpath in moreimportpaths:
-            commands.append('--paths ' + importpath)
-    if morehiddenimports:
-        for module in morehiddenimports:
-            commands.append('--hidden-import ' + module)
-    if excludemodules:
-        for module in excludemodules:
-            commands.append('--exclude-module ' + module)
-    
-    if runtimetmpdir is not None:
-        commands.append('--runtime-tmpdir ' + runtimetmpdir)
+        if self.filestyle == 'onefile':
+            self.commands.append('--onefile')
+        elif self.filestyle == 'onedir':
+            self.commands.append('--onedir')
+        else:
+            raise ValueError('filestyle must be onefile or onedir')
 
-    #PyInstaller.__main__.run(commands)
-    commands = ['pyinstaller'] + commands
-    commands = ' '.join(commands)
-    #print(commands)
-    subprocess.run(commands, cwd=path)
+        if self.distpath is not None:
+            self.commands.append('--distpath ' + self.distpath)
+        if self.workpath is not None:
+            self.commands.append('--workpath ' + self.workpath)
+        if self.specpath is not None:
+            self.commands.append('--specpath ' + self.specpath)
+
+        if self.coverdist:
+            self.commands.append('-y')
+        if self.coverwork:
+            self.commands.append('-clean')
+        
+        if self.loglevel != 'INFO':
+            self.commands.append('--log-level ' + self.loglevel)
+
+        if self.datas:
+            for data in self.datas:
+                self.commands.append(f'--add-data "{data[0]};{data[1]}"')
+        if self.binaries:
+            for binary in self.binaries:
+                self.commands.append(f'--add-binary "{binary[0]};{binary[1]}"')
+        
+        if self.moreimportpaths:
+            for importpath in self.moreimportpaths:
+                self.commands.append('--paths ' + importpath)
+        if self.morehiddenimports:
+            for module in self.morehiddenimports:
+                self.commands.append('--hidden-import ' + module)
+        if self.excludemodules:
+            for module in self.excludemodules:
+                self.commands.append('--exclude-module ' + module)
+        
+        if self.runtimetmpdir is not None:
+            self.commands.append('--runtime-tmpdir ' + self.runtimetmpdir)
+
+        # 执行命令
+        #PyInstaller.__main__.run(self.commands)
+        self.commands = ['pyinstaller'] + self.commands
+        self.commands = ' '.join(self.commands)
+        #print(self.commands)
+        subprocess.run(self.commands, **self.cwd)
+
+    def copy(self, src: str):
+        '''复制文件或文件夹到指定位置
+        弥补pyinstaller的不足
+        '''
+
+        # 生成目标路径
+        if self.name is not None:
+            name = self.name
+        else:
+            name = os.path.basename(self.pythonfile).split('.')[0]
+
+        if self.distpath is not None:
+            dst = os.path.join(self.distpath, name)
+        else:
+            dst = os.path.join(self.path, 'dist', name)
+
+        dst = os.path.join(dst, os.path.basename(src))
+        
+        # 复制
+        if os.path.isfile(src):
+            shutil.copyfile(src, dst)
+        elif os.path.isdir(src):
+            shutil.copytree(src, dst)
+        else:
+            raise ValueError('src must be a file or a directory')
--- test_pack.py
+++ test_pack.py
@@ -1,38 +1,68 @@
 import os
+import shutil
 import time
 import unittest
 from pack import Pack
 
-t = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
 
 class TestPack(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
 
-        self.path = r'D:\jyb\MyProgram\PyInstallerGUI\test_results\\' + t
-        os.makedirs(self.path)
+        # 创建文件夹run_results
+        cls.root = r'D:\jyb\MyProgram\PyInstallerGUI\run_results'
+        if not os.path.exists(cls.root):
+            os.makedirs(cls.root)
 
-        self.args1 = (r'D:\jyb\MyProgram\BallRunning\main.py',
-                      {'path' : self.path,
+        # 创建文件夹
+        cls.time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+        cls.path = os.path.join(cls.root, cls.time)
+        while os.path.exists(cls.path):
+            cls.time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+            cls.path = os.path.join(cls.root, cls.time)
+        os.makedirs(cls.path)
+
+        # 删除过多的文件夹
+        dirs = os.listdir(cls.root)
+        if len(dirs) > 5:
+            for dir in dirs[:-5]:
+                shutil.rmtree(os.path.join(cls.root, dir))
+
+        # 定义参数
+        cls.args1 = (r'D:\jyb\MyProgram\BallRunning\main.py',
+                      {'path' : cls.path,
                        'name' : 'BallRunning',
                        'icon' : r'D:\jyb\MyProgram\BallRunning\files\ricon.ico',
                        'nonconsole' : True,
                        'datas' : [(r'D:\jyb\MyProgram\BallRunning\files', r'.\files'),
-                                  (r'D:\jyb\MyProgram\BallRunning\levels', r'.\levels')]}
+                                  (r'D:\jyb\MyProgram\BallRunning\levels', r'.\levels')],
+                       'loglevel' : 'ERROR'}
                       )
         
-        self.args2 = (r'D:\jyb\MyProgram\FunctionGraphPainter\函数图像绘制器.pyw', 
-                      {'path' : self.path,
+        cls.args2 = (r'D:\jyb\MyProgram\FunctionGraphPainter\函数图像绘制器.pyw', 
+                      {'path' : cls.path,
                        'name' : 'FunctionGraphPainter',
                        'icon' : r'D:\jyb\MyProgram\FunctionGraphPainter\文件\图标.ico',
                        'nonconsole' : True,
-                       'datas' : [(r'D:\jyb\MyProgram\FunctionGraphPainter\文件', r'.\文件')]}
+                       'datas' : [(r'D:\jyb\MyProgram\FunctionGraphPainter\文件', r'.\文件')],
+                       'loglevel' : 'ERROR'}
                       )
 
-    def test_pack(self):
+        # 创建Pack对象
+        cls.pack1 = Pack(cls.args1[0], **cls.args1[1])
+        cls.pack2 = Pack(cls.args2[0], **cls.args2[1])
 
-        Pack(self.args1[0], **self.args1[1])
-        Pack(self.args2[0], **self.args2[1])
+    def test_pack_and_copy(self):
+        
+        # 打包
+        self.pack1.pack()
+        self.pack2.pack()
+
+        # 复制
+        self.pack1.copy(r'D:\jyb\MyProgram\BallRunning\files')
+        self.pack1.copy(r'D:\jyb\MyProgram\BallRunning\levels')
+        self.pack2.copy(r'D:\jyb\MyProgram\FunctionGraphPainter\文件')
 
 if __name__ == '__main__':
     unittest.main()
