1502f1592f91aeb2d6cb4d2e8896f15fa4fb23c4
j1y2b3 <2162539907@qq.com>
2025-02-03 14:08:46 +0800

    提交更改文件

--- tests/examples/ex_tkinter/gui/__init__.py
+++ tests/examples/ex_tkinter/gui/__init__.py
@@ -9,7 +9,7 @@ class Win:
     def __init__(self):
 
         self.win=tk.Tk()
-        self.win.title('example1')
+        self.win.title('tkinter example')
         self.win.geometry('700x300')
         self.win.resizable(False,False)
         self.win.iconbitmap(default=data.get_icon_path())
--- tests/test_pack.py
+++ tests/test_pack.py
@@ -7,25 +7,6 @@ from pack import Pack
 
 class TestPack(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-
-        # 生成文件夹
-        cls.root = r'D:\jyb\MyProgram\PyInstallerGUI\tests'
-        cls.srcpath = os.path.join(cls.root, 'examples')
-        cls.runpath = cls.makeResultDir(cls)
-
-        # 定义参数
-        cls.args1 = (os.path.join(cls.srcpath, r'ex_tkinter\main.py'),
-                      {'path' : cls.runpath,
-                       'name' : 'ex_tkinter',
-                       'datas' : [(os.path.join(cls.srcpath, r'ex_tkinter\datas'), r'.\datas')],
-                       'loglevel' : 'ERROR'}
-                     )
-
-        # 创建Pack对象
-        cls.pack1 = Pack(cls.args1[0], **cls.args1[1])
-
     def makeResultDir(self):
         
         # 确保存在results文件夹
@@ -49,10 +30,39 @@ class TestPack(unittest.TestCase):
         
         return resultpath
 
+    @classmethod
+    def setUpClass(cls):
+
+        # 生成文件夹
+        cls.root = r'D:\jyb\MyProgram\PyInstallerGUI\tests'
+        cls.srcpath = os.path.join(cls.root, 'examples')
+        cls.runpath = cls.makeResultDir(cls)
+
+        # 定义参数
+        cls.args1 = (os.path.join(cls.srcpath, r'ex_tkinter\main.py'),
+                      {'path' : cls.runpath,
+                       'name' : 'ex_tkinter',
+                       'datas' : [(os.path.join(cls.srcpath, r'ex_tkinter\datas'), r'.\datas')],
+                       'loglevel' : 'ERROR'}
+                     )
+        cls.args2 = (os.path.join(cls.srcpath, r'ex_pygame\main.py'),
+                      {'path' : cls.runpath,
+                       'name' : 'ex_pygame',
+                       'icon' : os.path.join(cls.srcpath, r'ex_pygame\datas\icon.ico'),
+                       'nonconsole' : True,
+                       'datas' : [(os.path.join(cls.srcpath, r'ex_pygame\datas'), r'.\datas')],
+                       'loglevel' : 'ERROR'}
+                     )
+
+        # 创建Pack对象
+        cls.pack1 = Pack(cls.args1[0], **cls.args1[1])
+        cls.pack2 = Pack(cls.args2[0], **cls.args2[1])
+
     def test_pack_and_copy(self):
         
         # 打包
         self.pack1.pack()
+        self.pack2.pack()
 
         # 复制
 
