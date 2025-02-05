8015e74df7833ad0bfafe3a9487355168a90b14d
j1y2b3 <2162539907@qq.com>
2025-02-05 13:52:21 +0800

    增加资料获取代码

--- data.py
+++ data.py
@@ -1,13 +1,32 @@
 '''获取datas文件夹内的文件'''
 
 import os
+import json
 
-def get_icon_path(style: str = 'console') -> str:
+root = os.path.join(os.path.dirname(__file__), 'datas')
+
+def getIconPath(style: str = 'console') -> str:
     '''获取图标路径'''
 
     if style == 'console':
-        return os.path.join(os.path.dirname(__file__), r'datas\icon-console.ico')
+        return os.path.join(root, r'icon-console.ico')
     elif style == 'windowed':
-        return os.path.join(os.path.dirname(__file__), r'datas\icon-windowed.ico')
+        return os.path.join(root, r'icon-windowed.ico')
     else:
         raise ValueError('style must be "console" or "windowed"')
+
+def getRecentPaths(file: str = 'recent.json') -> tuple[str]:
+    '''获取最近打开的Python文件路径'''
+
+    path = os.path.join(root, file)
+    if not os.path.exists(path):
+        return ()
+    with open(path, 'r', encoding='utf-8') as file:
+        return tuple(json.load(file))
+
+def writeRecentPaths(paths: tuple[str], file: str = 'recent.json'):
+    '''写入最近打开的Python文件路径'''
+
+    path = os.path.join(root, file)
+    with open(path, 'w', encoding='utf-8') as file:
+        json.dump(paths, file, ensure_ascii=False, indent=4)
